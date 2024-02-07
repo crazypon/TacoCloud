a0c89efa082e30093e4d6a01440e9e87cf53506b
Ilnur Fanisovich <ilnuryamaletdinov349@gmail.com>
2024-02-07 13:07:54 +0500

    before going to charvak

--- taco-cloud/src/main/java/sia/tacocloud/Taco.java
+++ taco-cloud/src/main/java/sia/tacocloud/Taco.java
@@ -1,21 +1,41 @@
 package sia.tacocloud;
 
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 
 
-
 @Data
+@Entity
 public class Taco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+    
+    private Date createdAt = new Date();
 
     @NotNull
     @Size(min=1, message="Choose at least one ingredient")
-    private List<Ingredient> ingredients;
+    @ManyToMany
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
+
 }
--- taco-cloud/src/main/java/sia/tacocloud/data/IngredientRepository.java
+++ taco-cloud/src/main/java/sia/tacocloud/data/IngredientRepository.java
@@ -1,10 +1,9 @@
 package sia.tacocloud.data;
-import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
 
 import sia.tacocloud.Ingredient;
 
-public interface IngredientRepository {
-    Iterable<Ingredient> findAll();
-    Optional<Ingredient> findById(String id);
-    Ingredient save(Ingredient ingredient);
-}
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+
+} 
